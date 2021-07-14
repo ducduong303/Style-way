583ab76be785706c5c71a711def91b4165ca0315
ducduong303 <57469702+ducduong303@users.noreply.github.com>
2021-07-14 14:04:57 +0700

    commit

--- server.js
+++ server.js
@@ -2,28 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-var cookieParser = require('cookie-parser')
 const userRouter = require('./routes/userRouter')
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(cookieParser())
-
-app.use("/users", userRouter)
+const noteRouter = require('./routes/noteRouter')
 const path = require('path')
 
-// Router
-// app.use("/user", require('./routes/userRouter'))
-// app.use("/user/image", require('./routes/upload'))
-// app.use("/user/category", require('./routes/categoryRouter'))
-// app.use("/user/size", require('./routes/sizeRouter'))
-// app.use("/user/color", require('./routes/colorRouter'))
-// app.use("/user/products", require('./routes/productRouter'))
-// app.use("/user/oder", require('./routes/oderRouter'))
-// app.use("/user/banner", require('./routes/bannerRouter'))
-// app.use("/user/blog", require('./routes/blogRouter'))
 
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+// Routes
+app.use('/users', userRouter)
+app.use('/api/notes', noteRouter)
 
 
 // Connect to MongoDB
@@ -33,18 +24,22 @@ mongoose.connect(URI, {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, err => {
-    if (err) return err;
+}, err =>{
+    if(err) throw err;
     console.log('Connected to MongoDB')
 })
 
+
 // Below MongoDB and  Above Listen Sever
-if (process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
+    app.get('*', (req, res) =>{
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     });
 }
+
+
+
 
 // Listen Server
 const PORT = process.env.PORT || 5000
