4985bb21222ee10eab023be88fd23cd402fda73e
ducduong303 <57469702+ducduong303@users.noreply.github.com>
2021-07-14 17:06:01 +0700

    commit

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import myReducer from "./reducers/index";
-
+import { composeWithDevTools } from 'redux-devtools-extension'
 const store = createStore(
     myReducer,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    composeWithDevTools(applyMiddleware(thunk))
+    // compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 )
 export default store
